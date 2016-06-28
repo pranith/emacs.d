69d1a528a8add47528c6dd79c72a63890528e600
Pranith Kumar <bobby.prani@gmail.com>
2016-06-28 10:40:06 -0400

    Update packages

---
+++ elpa/helm-mu-20160531.1031/helm-mu-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "helm-mu" "20160531.1031" "Helm sources for searching emails and contacts" '((helm "1.5.5")) :url "https://github.com/emacs-helm/helm-mu")
