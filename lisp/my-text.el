21d3793135a6cd9e717265fbecf4085dc7aa7786
Pranith Kumar <bobby.prani@gmail.com>
2017-04-21 13:24:29 -0400

    Add langtool for grammar check

--- lisp/my-text.el
+++ lisp/my-text.el
@@ -29,4 +29,12 @@
 (global-set-key "\C-c\C-gg" 'writegood-grade-level)
 (global-set-key "\C-c\C-ge" 'writegood-reading-ease)
 
+(require 'langtool)
+(setq langtool-language-tool-jar "/home/pranith/langtool/languagetool-commandline.jar")
+
+(global-set-key "\C-x4w" 'langtool-check-buffer)
+(global-set-key "\C-x4W" 'langtool-check-done)
+(global-set-key "\C-x4n" 'langtool-goto-next-error)
+(global-set-key "\C-x4p" 'langtool-goto-previous-error)
+(global-set-key "\C-x44" 'langtool-show-message-at-point)
 (provide 'my-text)
