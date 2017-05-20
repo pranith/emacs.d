8c0e1d77b3e16da01a8d6c49ebd1f49dd94ebc70
Pranith Kumar <bobby.prani@gmail.com>
2017-05-20 10:55:41 -0400

    Fix switching to log file

--- lisp/my-log.el
+++ lisp/my-log.el
@@ -1,5 +1,7 @@
 ; log file
-(find-file "/home/pranith/devops/Dropbox/research/work/diary.org")
-(define-key global-map "\C-cd" (lambda () (interactive) (switch-to-buffer "diary.org")))
+(defvar my-log-diary_file)
+(setq my-log-diary_file "/home/pranith/devops/Dropbox/research/work/diary.org")
+(find-file my-log-diary_file)
+(define-key global-map "\C-cd" (lambda () (interactive) (switch-to-buffer (find-file-noselect my-log-diary_file))))
 
 (provide 'my-log)
