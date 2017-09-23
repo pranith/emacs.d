e10e98173a98a3a866fd8ebbb450d31bdab9903b
Pranith Kumar <bobby.prani@gmail.com>
2017-09-23 10:43:10 -0400

    Increase custom font size

--- lisp/my-custom-theme.el
+++ lisp/my-custom-theme.el
@@ -56,4 +56,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Monaco" :foundry "apple" :slant normal :weight normal :height 113 :width normal)))))
+ '(default ((t (:family "Monaco" :foundry "apple" :slant normal :weight normal :height 122 :width normal)))))
